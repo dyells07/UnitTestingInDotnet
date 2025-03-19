f9448d80a3bc182c395fe7a186ffa5c92345810e
Bipin Khanal <76544001+dyells07@users.noreply.github.com>
2025-03-19 23:36:04 +0545

    Update DateTimeExtension.cs

--- RunGroop/RunGroop/RunGroopWebApp.Scraper/Extensions/DateTimeExtension.cs
+++ RunGroop/RunGroop/RunGroopWebApp.Scraper/Extensions/DateTimeExtension.cs
@@ -12,8 +12,7 @@ namespace RunGroopWebApp.Scraper.Extensions
         public static DateTime? ToDate(this string dateTimeStr, params string[] dateFmt)
         {
             // example: var dt = "2011-03-21 13:26".ToDate(new string[]{"yyyy-MM-dd HH:mm", 
-            //                                                  "M/d/yyyy h:mm:ss tt"});
-            // or simpler: 
+                                       
             // var dt = "2011-03-21 13:26".ToDate("yyyy-MM-dd HH:mm", "M/d/yyyy h:mm:ss tt");
             const DateTimeStyles style = DateTimeStyles.AllowWhiteSpaces;
             if (dateFmt == null)
